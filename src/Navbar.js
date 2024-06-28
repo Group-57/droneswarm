7641e3326517bdeea2872994f72fbf8ab8fa1137
Mohammed Shahed <146975293+MOHAMMEDSHAHED786@users.noreply.github.com>
2024-06-28 14:47:51 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,180 +1,3 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import './App.css'
-// function Navbar() {
-//   return (
-//     <nav>
-//       <ul class="nav nav-tabs">
-//   <li class="nav-item">
-//     <a class="nav-link active" aria-current="page" href="#">Active</a>
-//   </li>
-//   <li class="nav-item dropdown">
-//     <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
-//     <ul class="dropdown-menu">
-//       <li><a class="dropdown-item" href="#">Action</a></li>
-//       <li><a class="dropdown-item" href="#">Another action</a></li>
-//       <li><a class="dropdown-item" href="#">Something else here</a></li>
-//       {/* <li><hr class="dropdown-divider"></li> */}
-//       <li><a class="dropdown-item" href="#">Separated link</a></li>
-//     </ul>
-//   </li>
-//   <li class="nav-item">
-//     <a class="nav-link" href="#">Link</a>
-//   </li>
-//   <li class="nav-item">
-//     <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-//   </li>
-// </ul>
-
-
-//       <ul>
-//       <li>
-//           <Link to="/maps">PLANNER</Link>
-//         </li>
-//         <li>
-//           <Link to="/">Home</Link>
-//         </li>
-        
-//       </ul>
-//     </nav>
-
-
-
-  
-//   );
-// }
-
-// export default Navbar;
-
-
-
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import './Navbar.css'; // Import your CSS file for Navbar styling
-
-// function Navbar() {
-//   return (
-//     <nav className="navbar">
-//       <ul className="nav nav-tabs">
-//         <li className="nav-item">
-//           <a className="nav-link active" aria-current="page" href="#">Active</a>
-//         </li>
-//         <li className="nav-item dropdown">
-//           <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
-//           <ul className="dropdown-menu">
-//             <li><a className="dropdown-item" href="#">Plans</a></li>
-//             <li><a className="dropdown-item" href="#">suggestion</a></li>
-//             <li><Link to="/maps">PLANNER</Link></li>
-//             {/* <li><hr className="dropdown-divider"></li> */}
-//             <li><a className="dropdown-item" href="#">documentation</a></li>
-//           </ul>
-//         </li>
-//         <li className="nav-item">
-//           <a className="nav-link" href="#">Link</a>
-//         </li>
-//         <li className="nav-item">
-//           <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-//         </li>
-//       </ul>
-//       <ul className="nav-links">
-//         <li>
-//           <Link to="/maps">PLANNER</Link>
-//         </li>
-//         <li>
-//           <Link to="/">Home</Link>
-//         </li>
-//       </ul>
-//     </nav>
-//   );
-// }
-
-// export default Navbar;
-
-
-
-// import React, { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import { Dropdown } from 'react-bootstrap'; 
-
-
-// const Navbar = () => {
-//   const navigate = useNavigate();
-//   const [collapseOpen, setCollapseOpen] = useState(false);
-
-//   const handleNavLinkClick = (path) => {
-//     navigate(path);
-//     setCollapseOpen(false);
-//   };
-
-//   return (
-//     <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top" >
-//       <div className="container-fluid" style={{backgroundColor:'gray'}}>
-//         <a className="navbar-brand" onClick={() => handleNavLinkClick('/silver')} style={{cursor: 'pointer'}}>
-//           <img src="" style={{ maxHeight: '70px', width: '50px', maxWidth: '100%', height: 'auto', marginLeft: '5px' ,background:'transparent',borderRadius:'10px',padding:'4px',boxShadow: 'rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px',}} alt="Navbar Logo" />
-//         </a>
-//         <button className="navbar-toggler" type="button" onClick={() => setCollapseOpen(!collapseOpen)}>
-//           <span className="navbar-toggler-icon"></span>
-//         </button>
-//         <div className={`collapse navbar-collapse ${collapseOpen ? 'show' : ''}`} id="navbarNav">
-//           <ul className="navbar-nav mr-auto">
-
-//             <Dropdown style={{marginLeft:'10px',backgroundColor:'transparent'}}>
-//               <Dropdown.Toggle variant="transparent" id="dropdown-basic" className="nav-link active" style={{paddingTop:'10px',paddingLeft:'0px',marginRight:'10px',color:'#F5F5DC'}}>
-//                 1
-//               </Dropdown.Toggle>
-
-//               <Dropdown.Menu style={{width:'100%',minWidth:'200px' ,backgroundColor:'#5D54A4'}}>
-//                 <Dropdown.Item onClick={() => handleNavLinkClick('/silver')} style={{color:'#FFFFCC',backgroundColor:'#5D54A4'}}>1</Dropdown.Item>
-//                 <Dropdown.Item onClick={() => handleNavLinkClick('/silverstock')} style={{color:'#FFFFCC',backgroundColor:'#5D54A4'}}>2</Dropdown.Item>
-//               </Dropdown.Menu>
-//             </Dropdown>
-
-//             <Dropdown style={{marginLeft:'10px',backgroundColor:'transparent'} }>
-//               <Dropdown.Toggle variant="transparent" id="dropdown-basic" className="nav-link active" style={{paddingTop:'10px',paddingLeft:'0px',marginRight:'10px',color:'#F5F5DC'}}>
-//                 2
-//               </Dropdown.Toggle>
-//               <Dropdown.Menu style={{width:'100%',minWidth:'200px',backgroundColor:'#5D54A4',}}>
-//                 <Dropdown.Item onClick={() => handleNavLinkClick('/gold')} style={{color:'#FFFFCC',backgroundColor:'#5D54A4'}}>1</Dropdown.Item>
-//                 <Dropdown.Item onClick={() => handleNavLinkClick('/goldstock')} style={{color:'#FFFFCC',backgroundColor:'#5D54A4'}}>2</Dropdown.Item>
-//               </Dropdown.Menu>
-//             </Dropdown>            
-
-//             <Dropdown style={{marginLeft:'10px',backgroundColor:'transparent'}}>
-//               <Dropdown.Toggle variant="transparent" id="dropdown-basic" className="nav-link active" style={{paddingTop:'10px',color:'#F5F5DC',paddingLeft:'0px',marginRight:'10px'}}>
-//               3
-//               </Dropdown.Toggle>
-
-//               <Dropdown.Menu style={{width:'100%',minWidth:'200px',backgroundColor:'#5D54A4'}}>
-//                 <Dropdown.Item onClick={() => handleNavLinkClick('/dropdown-action-1')} style={{color:'#FFFFCC',backgroundColor:'#5D54A4'}}>1</Dropdown.Item>
-//                 <Dropdown.Item onClick={() => handleNavLinkClick('/dropdown-action-2')} style={{color:'#FFFFCC',backgroundColor:'#5D54A4'}}>2</Dropdown.Item>
-//                 <Dropdown.Item onClick={() => handleNavLinkClick('/dropdown-action-3')} style={{color:'#FFFFCC',backgroundColor:'#5D54A4'}}>3</Dropdown.Item>
-//                 <Dropdown.Item onClick={() => handleNavLinkClick('/dropdown-action-3')} style={{color:'#FFFFCC',backgroundColor:'#5D54A4'}}>4</Dropdown.Item>
-//                 <Dropdown.Item onClick={() => handleNavLinkClick('/dropdown-action-3')} style={{color:'#FFFFCC',backgroundColor:'#5D54A4'}}>5</Dropdown.Item>
-//               </Dropdown.Menu>
-//             </Dropdown>
-
-//             <li className="nav-item" style={{marginRight:'10px'}}>
-//               <button className="nav-link active" style={{margin:'0px',color:'#F5F5DC'}} onClick={() => handleNavLinkClick('/orders')}>4</button>
-//             </li>
-
-//             <li className="nav-item" style={{marginRight:'10px'}}>
-//               <button className="nav-link active" style={{margin:'0px',color:'#F5F5DC'}} onClick={() => handleNavLinkClick('/price')}>5</button>
-//             </li>
-
-//             <li className="nav-item" style={{marginRight:'10px'}}>
-//               <button className="nav-link active" style={{margin:'0px',color:'#F5F5DC'}} onClick={() => handleNavLinkClick('/report')}>6</button>
-//             </li>
-//             <li className="nav-item">
-//               <button className="nav-link active" style={{margin:'0px',color:'#F5F5DC'}} onClick={() => handleNavLinkClick('/invoice')}>Logout</button>
-//             </li>
-//           </ul>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
 // import React, { useState } from 'react';
 // import { useNavigate } from 'react-router-dom';
 
@@ -195,7 +18,7 @@
 //           onClick={() => handleNavLinkClick('/')}
 //           style={{ cursor: 'pointer' }}
 //         >
-//          PlanIt
+//          Planning It
 //         </a>
 //         <button
 //           className="navbar-toggler"
@@ -230,7 +53,7 @@
 //                 style={{ margin: '0px', color: '#F5F5DC' }}
 //                 onClick={() => handleNavLinkClick('/s4')}
 //               >
-                
+//                 Animation
 //               </button>
 //             </li>
 //             <li className="nav-item">
@@ -239,26 +62,15 @@
 //                 style={{ margin: '0px', color: '#F5F5DC' }}
 //                 onClick={() => handleNavLinkClick('/s5')}
 //               >
-//                 s5
+//                 map
 //               </button>
 //             </li>
-//             <li className="nav-item">
-//               <button
-//                 className="nav-link active"
-//                 style={{ margin: '0px', color: '#F5F5DC' }}
-//                 onClick={() => handleNavLinkClick('/s6')}
-//               >
-//                 {/* s6
-//               </button>
-//             </li>
-            
-      
 //             <li className="nav-item">
 //               <button
 //                 className="nav-link active"
 //                 style={{ margin: '0px', color: '#F5F5DC' }}
 //                 onClick={() => handleNavLinkClick('/invoice')}
-//               > */}
+//               >
 //                 Logout
 //               </button>
 //             </li>
@@ -270,27 +82,237 @@
 // }
 
 // export default Navbar;
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+// import "./Navbar.css"
+
+// function Navbar() {
+//   const navigate = useNavigate();
+//   const [collapseOpen, setCollapseOpen] = useState(false);
+
+//   const handleNavLinkClick = (path) => {
+//     navigate(path);
+//     setCollapseOpen(false);
+//   };
+
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+//       <div className="container-fluid" style={{ backgroundColor: 'black' }}>
+//         <a
+//           className="navbar-brand"
+//           onClick={() => handleNavLinkClick('/')}
+//           style={{ cursor: 'pointer', color: 'gold' }}
+//         >
+//           SWARM PLANNER
+//         </a>
+//         <button
+//           className="navbar-toggler"
+//           type="button"
+//           onClick={() => setCollapseOpen(!collapseOpen)}
+//         >
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+//         <div className={`collapse navbar-collapse ${collapseOpen ? 'show' : ''}`} id="navbarNav">
+//           <ul className="navbar-nav mr-auto">
+//             <li className="nav-item">
+//               <button
+//                 className="nav-link"
+//                 style={{ margin: '0px', color: '#F5F5DC' }}
+//                 onClick={() => handleNavLinkClick('/s2')}
+//               >
+//                 DIY
+//               </button>
+//             </li>
+//             <li className="nav-item">
+//               <button
+//                 className="nav-link"
+//                 style={{ margin: '0px', color: '#F5F5DC' }}
+//                 onClick={() => handleNavLinkClick('/k')}
+//               >
+//                 TEST
+//               </button>
+//             </li>
+//             <li className="nav-item">
+//               <button
+//                 className="nav-link"
+//                 style={{ margin: '0px', color: '#F5F5DC' }}
+//                 onClick={() => handleNavLinkClick('/s4')}
+//               >
+//                 Animation
+//               </button>
+//               {/* <Link to = "/s4" className='test-link' style={{background:"black", color:'white'}}> new link</Link> */}
+//             </li>
+//             <li className="nav-item">
+//               <button
+//                 className="nav-link"
+//                 style={{ margin: '0px', color: '#F5F5DC' }}
+//                 onClick={() => handleNavLinkClick('/s5')}
+//               >
+//                 map
+//               </button>
+//             </li>
+//             <li className="nav-item">
+//               <button
+//                 className="nav-link"
+//                 style={{ margin: '0px', color: '#F5F5DC' }}
+//                 onClick={() => handleNavLinkClick('/invoice')}
+//               >
+//                 Logout
+//               </button>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
+
+
+// import React, { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import './Navbar.css'; // Ensure this path is correct
+
+// function Navbar() {
+//   const navigate = useNavigate();
+//   const [collapseOpen, setCollapseOpen] = useState(false);
+
+//   const handleNavLinkClick = (path) => {
+//     navigate(path);
+//     setCollapseOpen(false);
+//   };
+
+//   return (
+//     <nav className="navbar navbar-expand-lg fixed-top">
+//       <div className="container-fluid">
+//         <a
+//           className="navbar-brand"
+//           onClick={() => handleNavLinkClick('/')}
+//           style={{ cursor: 'pointer', color: 'gold' }}
+//         >
+//          SWARM PLANNER
+//         </a>
+//         <button
+//           className="navbar-toggler"
+//           type="button"
+//           onClick={() => setCollapseOpen(!collapseOpen)}
+//         >
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+//         <div className={`collapse navbar-collapse ${collapseOpen ? 'show' : ''}`} id="navbarNav">
+//           <ul className="navbar-nav mr-auto">
+//             <li className="nav-item">
+//               <button
+//                 className="nav-link active"
+//                 style={{ cursor: 'pointer', color: '#FFF' }}
+//                 onClick={() => handleNavLinkClick('/s2')}
+//               >
+//                 DIY
+//               </button>
+//             </li>
+//             <li className="nav-item">
+//               <button
+//                 className="nav-link active"
+//                 onClick={() => handleNavLinkClick('/s3')}
+//                 style={{ cursor: 'pointer', color: '#FFF' }}
+//               >
+//                 TEST
+//               </button>
+//             </li>
+//             <li className="nav-item">
+//               <button
+//                 className="nav-link active"
+//                 onClick={() => handleNavLinkClick('/s4')}
+//                 style={{ cursor: 'pointer', color: '#FFF' }}
+//               >
+//                 Animation
+//               </button>
+//             </li>
+//             <li className="nav-item">
+//               <button
+//                 className="nav-link active"
+//                 onClick={() => handleNavLinkClick('/s5')}
+//                 style={{ cursor: 'pointer', color: '#FFF' }}
+//               >
+//                 map
+//               </button>
+//             </li>
+//             <li className="nav-item">
+//               <button
+//                 className="nav-link active"
+//                 onClick={() => handleNavLinkClick('/invoice')}
+//                 style={{ cursor: 'pointer', color: '#FFF' }}
+//               >
+//                 Logout
+//               </button>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
+
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import './Navbar.css'; // Ensure this path is correct
 
 function Navbar() {
   const navigate = useNavigate();
+  const location = useLocation();
   const [collapseOpen, setCollapseOpen] = useState(false);
+  const [navbarBackground, setNavbarBackground] = useState('transparent');
 
   const handleNavLinkClick = (path) => {
     navigate(path);
     setCollapseOpen(false);
   };
 
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setNavbarBackground('black');
+    } else {
+      setNavbarBackground('transparent');
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (location.pathname !== '/') {
+      setNavbarBackground('black');
+    } else {
+      setNavbarBackground(window.scrollY > 20 ? 'black' : 'transparent');
+    }
+  }, [location.pathname]);
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-      <div className="container-fluid" style={{ backgroundColor: 'gray' }}>
+    <nav className="navbar navbar-expand-lg fixed-top" style={{ backgroundColor: navbarBackground, transition: 'background-color 0.3s' }}>
+      <div className="container-fluid">
         <a
           className="navbar-brand"
           onClick={() => handleNavLinkClick('/')}
-          style={{ cursor: 'pointer' }}
+          style={{cursor: 'pointer', color: 'gold' }}
         >
-         PlanIt
+          SWARM PLANNER
         </a>
         <button
           className="navbar-toggler"
@@ -304,7 +326,7 @@ function Navbar() {
             <li className="nav-item">
               <button
                 className="nav-link active"
-                style={{ margin: '0px', color: '#F5F5DC' }}
+                style={{ marginRight:'0px',cursor: 'pointer', color: '#FFF' }}
                 onClick={() => handleNavLinkClick('/s2')}
               >
                 DIY
@@ -313,8 +335,8 @@ function Navbar() {
             <li className="nav-item">
               <button
                 className="nav-link active"
-                style={{ margin: '0px', color: '#F5F5DC' }}
                 onClick={() => handleNavLinkClick('/s3')}
+                style={{ cursor: 'pointer', color: '#FFF' }}
               >
                 TEST
               </button>
@@ -322,26 +344,26 @@ function Navbar() {
             <li className="nav-item">
               <button
                 className="nav-link active"
-                style={{ margin: '0px', color: '#F5F5DC' }}
                 onClick={() => handleNavLinkClick('/s4')}
+                style={{ cursor: 'pointer', color: '#FFF' }}
               >
-                s4
+                Animation
               </button>
             </li>
             <li className="nav-item">
               <button
                 className="nav-link active"
-                style={{ margin: '0px', color: '#F5F5DC' }}
-                onClick={() => handleNavLinkClick('/s5')}
+                onClick={() => handleNavLinkClick('/map')}
+                style={{ cursor: 'pointer', color: '#FFF' }}
               >
-                s5
+                map
               </button>
             </li>
             <li className="nav-item">
               <button
                 className="nav-link active"
-                style={{ margin: '0px', color: '#F5F5DC' }}
                 onClick={() => handleNavLinkClick('/invoice')}
+                style={{ cursor: 'pointer', color: '#FFF' }}
               >
                 Logout
               </button>
